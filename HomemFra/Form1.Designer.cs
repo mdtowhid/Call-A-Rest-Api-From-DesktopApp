6a4a62c5d62dcabb3981ca1c93b7ea36b40e1e55
Md_Towhid <towhid.cse2015to@gmail.com>
2021-09-18 01:59:04 +0600

    API Calling via Desktop app

---
+++ HomemFra/Form1.Designer.cs
@@ -0,0 +1,169 @@
+﻿
+namespace HomemFra
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.empNameTextBox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.deptNameTextBox = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.designationTextBox = new System.Windows.Forms.TextBox();
+            this.SaveEmployeeInfo = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 183);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(776, 255);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.DoubleClick += new System.EventHandler(this.dataGridView1_DoubleClick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.label1.Location = new System.Drawing.Point(12, 165);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(37, 15);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "label1";
+            // 
+            // empNameTextBox
+            // 
+            this.empNameTextBox.Location = new System.Drawing.Point(12, 33);
+            this.empNameTextBox.Name = "empNameTextBox";
+            this.empNameTextBox.Size = new System.Drawing.Size(183, 20);
+            this.empNameTextBox.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.label2.Location = new System.Drawing.Point(12, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(86, 15);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Employee Name";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label3.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.label3.Location = new System.Drawing.Point(12, 70);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(95, 15);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Department Name";
+            // 
+            // deptNameTextBox
+            // 
+            this.deptNameTextBox.Location = new System.Drawing.Point(12, 88);
+            this.deptNameTextBox.Name = "deptNameTextBox";
+            this.deptNameTextBox.Size = new System.Drawing.Size(183, 20);
+            this.deptNameTextBox.TabIndex = 4;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label4.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.label4.Location = new System.Drawing.Point(239, 53);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(65, 15);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Designation";
+            // 
+            // designationTextBox
+            // 
+            this.designationTextBox.Location = new System.Drawing.Point(239, 77);
+            this.designationTextBox.Name = "designationTextBox";
+            this.designationTextBox.Size = new System.Drawing.Size(183, 20);
+            this.designationTextBox.TabIndex = 6;
+            // 
+            // SaveEmployeeInfo
+            // 
+            this.SaveEmployeeInfo.Location = new System.Drawing.Point(12, 120);
+            this.SaveEmployeeInfo.Name = "SaveEmployeeInfo";
+            this.SaveEmployeeInfo.Size = new System.Drawing.Size(75, 23);
+            this.SaveEmployeeInfo.TabIndex = 8;
+            this.SaveEmployeeInfo.Text = "Save";
+            this.SaveEmployeeInfo.UseVisualStyleBackColor = true;
+            this.SaveEmployeeInfo.Click += new System.EventHandler(this.SaveEmployeeInfo_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.SaveEmployeeInfo);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.designationTextBox);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.deptNameTextBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.empNameTextBox);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridView1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Employee CRUD";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label1;
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.TextBox empNameTextBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox deptNameTextBox;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox designationTextBox;
+        private System.Windows.Forms.Button SaveEmployeeInfo;
+    }
+}
+
---
+++ HomemFra/Form1.cs
@@ -0,0 +1,122 @@
+﻿using Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HomemFra
+{
+    public partial class Form1 : Form
+    {
+        List<Employee> employees;
+        HttpClient client;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private async void Form1_Load(object sender, EventArgs e)
+        {
+            ReloadGridView(await GetEmployeesAsync());
+        }
+
+        private async Task<List<Employee>> GetEmployeesAsync()
+        {
+            client = new HttpClient();
+            List<Employee> employees = new List<Employee>();
+            client.BaseAddress = new Uri("http://localhost/DotnetTricksApi/api/");
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            var response = await client.GetStringAsync("employee/getall");
+            employees = JsonConvert.DeserializeObject<List<Employee>>(response);
+            client = null;
+            return employees;
+        }
+
+        private void dataGridView1_CellMouseDown_1(object sender, DataGridViewCellMouseEventArgs e)
+        {
+            foreach (DataGridViewRow row in dataGridView1.SelectedRows)
+            {
+                string value1 = row.Cells[0].Value.ToString();
+                string value2 = row.Cells[1].Value.ToString();
+                //...
+            }
+        }
+
+        private void dataGridView1_SelectionChanged(object sender, EventArgs e)
+        {
+            foreach (DataGridViewRow row in dataGridView1.SelectedRows)
+            {
+                string value1 = row.Cells[0].Value.ToString();
+                string value2 = row.Cells[1].Value.ToString();
+                //...
+            }
+        }
+
+        private async void dataGridView1_DoubleClick(object sender, EventArgs e)
+        {
+            var j = dataGridView1.CurrentRow.Index;
+            employees = await GetEmployeesAsync();
+            if (employees != null)
+            {
+                client = new HttpClient();
+                Employee emp = employees[j];
+                
+                
+            }
+
+
+        }
+
+        private async void SaveEmployeeInfo_Click(object sender, EventArgs e)
+        {
+            client = new HttpClient();
+            client.BaseAddress = new Uri("http://localhost/DotnetTricksApi/api/");
+            //client.DefaultRequestHeaders.Accept.Clear();
+            Employee employee = new Employee
+            {
+                EmpName = empNameTextBox.Text,
+                Department = deptNameTextBox.Text,
+                Designation = designationTextBox.Text
+            };
+            var myContent = JsonConvert.SerializeObject(employee);
+            var buffer = System.Text.Encoding.UTF8.GetBytes(myContent);
+            var byteContent = new ByteArrayContent(buffer);
+            byteContent.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+            var result = await client.PostAsync("employee/post", byteContent);
+            client = null;
+            ReloadGridView(await GetEmployeesAsync());
+        }
+
+        void ReloadGridView(List<Employee> employees)
+        {
+            DataTable dt = new DataTable();
+            dt.Columns.Add("EmpId");
+            dt.Columns.Add("EmpName");
+            dt.Columns.Add("Department");
+            dt.Columns.Add("Designation");
+            if (employees.Count > 0)
+            {
+                foreach (var item in employees)
+                {
+                    var row = dt.NewRow();
+                    row["EmpId"] = item.EmpId;
+                    row["EmpName"] = item.EmpName;
+                    row["Department"] = item.Department;
+                    row["Designation"] = item.Designation;
+                    dataGridView1.Tag = item;
+                    dt.Rows.Add(row);
+                }
+            }
+            dataGridView1.DataSource = dt;
+        }
+    }
+}
